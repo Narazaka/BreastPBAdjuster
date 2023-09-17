6ef19fc2af36c2e960faa200352ed5f304a839c2
Narazaka <info@narazaka.net>
2023-09-17 17:01:29 +0900

    impl

--- BreastPBAdjuster.cs
+++ BreastPBAdjuster.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections;
+﻿using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 using VRC.SDK3.Dynamics.PhysBone.Components;
 using VRC.SDKBase;
+using UnityEditor.Hardware;
+using System;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -18,8 +20,96 @@ namespace Narazaka.VRChat.BreastPBAdjuster
         [SerializeField]
         public Transform BreastR;
         [SerializeField]
-        float BaseBoneLength = 0.01f;
-        
+        float BaseBoneLength = 0.1f;
+        [SerializeField]
+        bool ChangeBreastSize = true;
+        [SerializeField]
+        List<BreastKeyFrame> KeyFrames = new List<BreastKeyFrame>();
+        BreastKeyFrame DefaultKeyFrame { get
+            {
+                var kf = KeyFrames.FirstOrDefault(f => f.IsDefault);
+                if (kf != null)
+                {
+                    return kf;
+                }
+                if (KeyFrames.Count == 0)
+                {
+                    KeyFrames.Add(new BreastKeyFrame());
+                }
+                kf = KeyFrames.First();
+                kf.IsDefault = true;
+                return kf;
+            }
+        }
+
+        [Serializable]
+        public class TransformMemo
+        {
+            public static void Set(SerializedProperty memo, Transform transform)
+            {
+                if (transform == null)
+                {
+                    memo.FindPropertyRelative(nameof(Position)).vector3Value = Vector3.zero;
+                    memo.FindPropertyRelative(nameof(Rotation)).quaternionValue = Quaternion.identity;
+                    memo.FindPropertyRelative(nameof(Scale)).vector3Value = Vector3.one;
+                }
+                else
+                {
+                    memo.FindPropertyRelative(nameof(Position)).vector3Value = transform.position;
+                    memo.FindPropertyRelative(nameof(Rotation)).quaternionValue = transform.rotation;
+                    memo.FindPropertyRelative(nameof(Scale)).vector3Value = transform.localScale;
+                }
+            }
+
+            public Vector3 Position;
+            public Quaternion Rotation;
+            public Vector3 Scale;
+
+            public void Set(Transform transform)
+            {
+                if (transform == null)
+                {
+                    Position = Vector3.zero;
+                    Rotation = Quaternion.identity;
+                    Scale = Vector3.one;
+                }
+                else
+                {
+                    Position = transform.position;
+                    Rotation = transform.rotation;
+                    Scale = transform.localScale;
+                }
+            }
+        }
+
+        [Serializable]
+        public class PhysBoneValue
+        {
+            public FloatValue Pull;
+            public FloatValue Momentum;
+            public FloatValue Stiffness;
+            public FloatValue Gravity;
+            public FloatValue GravityFalloff;
+            public FloatValue Immobile;
+            public FloatValue MaxAngle;
+            public FloatValue Radius;
+
+            [Serializable]
+            public class FloatValue
+            {
+                public bool IsOverride;
+                public float Value;
+            }
+        }
+
+        [Serializable]
+        public class BreastKeyFrame
+        {
+            public bool IsDefault;
+            public TransformMemo BreastL = new TransformMemo();
+            public TransformMemo BreastR = new TransformMemo();
+        }
+
 
         private void OnDrawGizmosSelected()
         {
@@ -57,10 +147,15 @@ namespace Narazaka.VRChat.BreastPBAdjuster
                     var rot = Quaternion.FromToRotation(Vector3.up, vec);
                     Start.position = startPosition;
                     Start.rotation = rot;
-                    Middle.position = startPosition + vec.normalized * baseBoneLength;
+                    Middle.position = startPosition + vec * baseBoneLength;
                     Middle.rotation = rot;
                     End.position = endPosition;
                     End.rotation = rot;
+                }
+
+                public void ChangeBaseBoneLength(float baseBoneLength)
+                {
+                    MoveEndPosition(End.position, baseBoneLength);
                 }
 
                 public void DrawGizmos()
@@ -112,11 +207,128 @@ namespace Narazaka.VRChat.BreastPBAdjuster
             BoneCache Bones { get => _Bones == null || !_Bones.Valid ? (_Bones = new BoneCache(BreastPBAdjuster.transform)) : _Bones; }
             BoneCache _Bones;
 
+            enum EditMode
+            {
+                Normal,
+                Squish,
+            }
+
+            EditMode Mode;
+            UnityEditorInternal.ReorderableList KeyFramesList;
+
+            public override void OnInspectorGUI()
+            {
+                serializedObject.Update();
+
+                var breastL = serializedObject.FindProperty(nameof(BreastL));
+                var breastR = serializedObject.FindProperty(nameof(BreastR));
+                var keyFrames = serializedObject.FindProperty(nameof(KeyFrames));
+                var baseBoneLength = serializedObject.FindProperty(nameof(BaseBoneLength));
+                var changeBreastSize = serializedObject.FindProperty(nameof(ChangeBreastSize));
+
+                var prevL = breastL.objectReferenceValue;
+                var prevR = breastR.objectReferenceValue;
+                var prevBaseBoneLength = baseBoneLength.floatValue;
+
+                EditorGUILayout.PropertyField(breastL);
+                EditorGUILayout.PropertyField(breastR);
+                EditorGUILayout.PropertyField(baseBoneLength);
+                EditorGUILayout.PropertyField(changeBreastSize);
+                if (KeyFramesList == null)
+                {
+                    KeyFramesList = new UnityEditorInternal.ReorderableList(serializedObject, keyFrames);
+                    KeyFramesList.elementHeightCallback = (i) =>
+                    {
+                        var keyFrame = keyFrames.GetArrayElementAtIndex(i);
+                        var keyFrameBreastL = keyFrame.FindPropertyRelative("BreastL");
+                        var keyFrameBreastR = keyFrame.FindPropertyRelative("BreastR");
+                        return (EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing) * 2 + EditorGUI.GetPropertyHeight(keyFrameBreastL) + EditorGUI.GetPropertyHeight(keyFrameBreastR);
+                    };
+                    KeyFramesList.drawElementCallback = (rect, index, isActive, isFocused) =>
+                    {
+                        var keyFrame = keyFrames.GetArrayElementAtIndex(index);
+                        rect.height = EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+                        EditorGUI.PropertyField(rect, keyFrame.FindPropertyRelative("IsDefault"));
+                        rect.y += rect.height;
+
+                        var keyFrameBreastL = keyFrame.FindPropertyRelative("BreastL");
+                        var keyFrameBreastR = keyFrame.FindPropertyRelative("BreastR");
+                        using (new EditorGUI.DisabledGroupScope(true))
+                        {
+                            rect.height = EditorGUI.GetPropertyHeight(keyFrameBreastL);
+                            EditorGUI.PropertyField(rect, keyFrameBreastL, true);
+                            rect.y += rect.height;
+                            rect.height = EditorGUI.GetPropertyHeight(keyFrameBreastR);
+                            EditorGUI.PropertyField(rect, keyFrameBreastR, true);
+                            rect.y += rect.height;
+                        }
+
+                        rect.height = EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+                        if (GUI.Button(rect, "Set current transforms"))
+                        {
+                            TransformMemo.Set(keyFrameBreastL, breastL.objectReferenceValue as Transform);
+                            TransformMemo.Set(keyFrameBreastR, breastR.objectReferenceValue as Transform);
+                        }
+                    };
+                }
+                KeyFramesList.DoLayoutList();
+
+                var nowL = breastL.objectReferenceValue;
+                var nowR = breastR.objectReferenceValue;
+                var nowBaseBoneLength = baseBoneLength.floatValue;
+
+                if (prevL != nowL)
+                {
+                    TransformMemo.Set(GetDefaultKeyFrameProperty().FindPropertyRelative(nameof(BreastL)), breastL.objectReferenceValue as Transform);
+                }
+                if (prevR != nowR)
+                {
+                    TransformMemo.Set(GetDefaultKeyFrameProperty().FindPropertyRelative(nameof(BreastR)), breastR.objectReferenceValue as Transform);
+                }
+                if (prevBaseBoneLength != nowBaseBoneLength)
+                {
+                    Bones.L.ChangeBaseBoneLength(nowBaseBoneLength);
+                    Bones.R.ChangeBaseBoneLength(nowBaseBoneLength);
+                    SetPB(Bones.L.PB.CalcRadius(0f), Bones.L.PB.radius, nowBaseBoneLength);
+                }
+                serializedObject.ApplyModifiedProperties();
+            }
+
+            SerializedProperty GetDefaultKeyFrameProperty()
+            {
+                var keyFrames = serializedObject.FindProperty(nameof(KeyFrames));
+
+                SerializedProperty keyFrame = null;
+                for (var i = 0; i < keyFrames.arraySize; i++)
+                {
+                    var kf = keyFrames.GetArrayElementAtIndex(i);
+                    if (kf.FindPropertyRelative("IsDefault").boolValue)
+                    {
+                        keyFrame = kf;
+                        break;
+                    }
+                }
+
+                if (keyFrame != null)
+                {
+                    return keyFrame;
+                }
+
+                if (keyFrames.arraySize == 0)
+                {
+                    keyFrames.InsertArrayElementAtIndex(0);
+                }
+                keyFrame = keyFrames.GetArrayElementAtIndex(0);
+                keyFrame.FindPropertyRelative("IsDefault").boolValue = true;
+                return keyFrame;
+            }
+
             private void OnSceneGUI()
             {
                 if (BreastPBAdjuster.BreastL != null)
                 {
-                    Bones.Breast_L.position = BreastPBAdjuster.BreastL.position;
+                    Bones.Breast_L.position = BreastPBAdjuster.DefaultKeyFrame.BreastL.Position;
+                    Bones.Breast_L.localScale = BreastPBAdjuster.DefaultKeyFrame.BreastL.Scale;
                     ManipulatePosition(Bones.L, Bones.R);
                     ManipulateScale(Bones.L, Bones.R);
                     ManipulatePB(Bones.L, Bones.R);
@@ -124,7 +336,8 @@ namespace Narazaka.VRChat.BreastPBAdjuster
                 }
                 if (BreastPBAdjuster.BreastR != null)
                 {
-                    Bones.Breast_R.position = BreastPBAdjuster.BreastR.position;
+                    Bones.Breast_R.position = BreastPBAdjuster.DefaultKeyFrame.BreastR.Position;
+                    Bones.Breast_R.localScale = BreastPBAdjuster.DefaultKeyFrame.BreastR.Scale;
                     ManipulatePosition(Bones.R, Bones.L);
                     ManipulateScale(Bones.R, Bones.L);
                     ManipulatePB(Bones.R, Bones.L);
@@ -140,7 +353,7 @@ namespace Narazaka.VRChat.BreastPBAdjuster
                     if (check.changed)
                     {
                         main.MoveEndPosition(endPosition, BreastPBAdjuster.BaseBoneLength);
-                        var center = (BreastPBAdjuster.BreastL.position + BreastPBAdjuster.BreastR.position) / 2;
+                        var center = (BreastPBAdjuster.DefaultKeyFrame.BreastL.Position + BreastPBAdjuster.DefaultKeyFrame.BreastR.Position) / 2;
                         endPosition = Vector3.Reflect(endPosition - center, Vector3.left) + center;
                         sub.MoveEndPosition(endPosition, BreastPBAdjuster.BaseBoneLength);
                     }
@@ -151,11 +364,11 @@ namespace Narazaka.VRChat.BreastPBAdjuster
             {
                 using (var check = new EditorGUI.ChangeCheckScope())
                 {
-                    var scale = Handles.ScaleHandle(main.Middle.localScale, main.Middle.position, main.Middle.rotation, 0.02f);
+                    var scale = Handles.ScaleHandle(main.Start.localScale, main.Start.position, main.Start.rotation, 0.02f);
                     if (check.changed)
                     {
-                        main.Middle.localScale = scale;
-                        sub.Middle.localScale = scale;
+                        main.Start.localScale = scale;
+                        sub.Start.localScale = scale;
                     }
                 }
             }
@@ -164,19 +377,25 @@ namespace Narazaka.VRChat.BreastPBAdjuster
             {
                 using (var check = new EditorGUI.ChangeCheckScope())
                 {
-                    var scale = main.Middle.localScale;
+                    var scale = main.Start.localScale;
                     var avgScale = (scale.x + scale.y + scale.z) / 3f;
-                    var radiusEnd = Handles.RadiusHandle(Quaternion.identity, main.End.position, main.PB.radius * avgScale);
-                    var radiusStart = Handles.RadiusHandle(Quaternion.identity, main.Middle.position, main.PB.CalcRadius(0.5f) * avgScale);
+                    var radiusEnd = Handles.RadiusHandle(Quaternion.identity, main.End.position, main.PB.radius * avgScale) / avgScale;
+                    var radiusStart = Handles.RadiusHandle(Quaternion.identity, main.Start.position, main.PB.CalcRadius(0f) * avgScale) / avgScale;
                     if (check.changed)
                     {
-                        var curve = new AnimationCurve(new Keyframe { time = 0, value = 0 }, new Keyframe { time = 0.5f, value = radiusStart / radiusEnd}, new Keyframe { time = 1, value = 1});
-                        main.PB.radius = radiusEnd / avgScale;
-                        main.PB.radiusCurve = curve;
-                        sub.PB.radius = radiusEnd / avgScale;
-                        sub.PB.radiusCurve = curve;
+                        SetPB(radiusStart, radiusEnd, BreastPBAdjuster.BaseBoneLength);
                     }
                 }
+            }
+
+            void SetPB(float radiusStart, float radiusEnd, float baseBoneLength)
+            {
+                var rate = radiusStart / radiusEnd;
+                var curve = new AnimationCurve(new Keyframe { time = 0f, value = rate }, new Keyframe { time = 0.5f, value = baseBoneLength + rate * (1 - baseBoneLength) }, new Keyframe { time = 1, value = 1 });
+                Bones.L.PB.radius = radiusEnd;
+                Bones.L.PB.radiusCurve = curve;
+                Bones.R.PB.radius = radiusEnd;
+                Bones.R.PB.radiusCurve = curve;
             }
         }
 #endif
---
+++ Editor/BreastPBAdjusterPlugin.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using nadena.dev.ndmf;
+using nadena.dev.ndmf.fluent;
+using UnityEngine.Animations;
+
+[assembly: ExportsPlugin(typeof(Narazaka.VRChat.BreastPBAdjuster.Editor.BreastPBAdjusterPlugin))]
+
+namespace Narazaka.VRChat.BreastPBAdjuster.Editor
+{
+    public class BreastPBAdjusterPlugin : Plugin<BreastPBAdjusterPlugin>
+    {
+        /// <summary>
+        /// This name is used to identify the plugin internally, and can be used to declare BeforePlugin/AfterPlugin
+        /// dependencies. If not set, the full type name will be used.
+        /// </summary>
+        public override string QualifiedName => "net.narazaka.vrchat.breast_pb_adjuster";
+
+        /// <summary>
+        /// The plugin name shown in debug UIs. If not set, the qualified name will be shown.
+        /// </summary>
+        public override string DisplayName => "BreastPBAdjuster";
+        protected override void Configure()
+        {
+            InPhase(BuildPhase.Resolving).Run("BreastPBAdjuster", ctx =>
+            {
+                var breastPBAdjuster = ctx.AvatarRootObject.GetComponentInChildren<BreastPBAdjuster>();
+                if (breastPBAdjuster == null) return;
+
+                ProcessBreast(breastPBAdjuster.BreastL, breastPBAdjuster.transform.Find("Breast_L"));
+                ProcessBreast(breastPBAdjuster.BreastR, breastPBAdjuster.transform.Find("Breast_R"));
+            });
+        }
+
+        void ProcessBreast(Transform avatarBreast, Transform targetBreast)
+        {
+            var r = avatarBreast.GetComponent<RotationConstraint>();
+            if (r == null) r = avatarBreast.gameObject.AddComponent<RotationConstraint>();
+            r.AddSource(new ConstraintSource { sourceTransform = targetBreast, weight = 1 });
+            var rotationDelta = Quaternion.Inverse(targetBreast.rotation) * avatarBreast.rotation;
+            r.rotationOffset = rotationDelta.eulerAngles;
+            r.constraintActive = true;
+
+            var s = avatarBreast.GetComponent<ScaleConstraint>();
+            if (s == null) s = avatarBreast.gameObject.AddComponent<ScaleConstraint>();
+            s.AddSource(new ConstraintSource { sourceTransform = targetBreast, weight = 1 });
+            s.scaleOffset = Vector3.one;
+            s.constraintActive = true;
+        }
+    }
+}
