6c433fea7391f29be0e34cb71848f8f97259271e
Narazaka <info@narazaka.net>
2023-09-17 11:35:30 +0900

    bone mod

--- BreastPBAdjuster.cs
+++ BreastPBAdjuster.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using VRC.SDK3.Dynamics.PhysBone.Components;
+using VRC.SDKBase;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -9,13 +10,13 @@ using UnityEditor;
 namespace Narazaka.VRChat.BreastPBAdjuster
 {
     [AddComponentMenu(nameof(BreastPBAdjuster))]
-    public class BreastPBAdjuster : MonoBehaviour
+    public class BreastPBAdjuster : MonoBehaviour, IEditorOnly
     {
 #if UNITY_EDITOR
         [SerializeField]
-        Transform BreastL;
+        public Transform BreastL;
         [SerializeField]
-        Transform BreastR;
+        public Transform BreastR;
         [SerializeField]
         float BaseBoneLength = 0.01f;
         
@@ -35,39 +36,41 @@ namespace Narazaka.VRChat.BreastPBAdjuster
 
             class BoneSet
             {
-                public readonly Transform Base;
                 public readonly Transform Start;
+                public readonly Transform Middle;
                 public readonly Transform End;
                 public readonly VRCPhysBone PB;
-                public BoneSet(Transform baseBone, Transform startBone, Transform endBone)
+                public BoneSet(Transform baseBone, Transform middleBone, Transform endBone)
                 {
-                    Base = baseBone;
-                    Start = startBone;
+                    Start = baseBone;
+                    Middle = middleBone;
                     End = endBone;
-                    PB = Base.GetComponent<VRCPhysBone>();
+                    PB = Start.GetComponent<VRCPhysBone>();
                 }
 
-                public bool Valid { get => Base != null && Start != null && End != null; }
+                public bool Valid { get => Start != null && Middle != null && End != null; }
 
                 public void MoveEndPosition(Vector3 endPosition, float baseBoneLength)
                 {
                     var startPosition = Start.position;
-                    Base.position = startPosition + (startPosition - endPosition).normalized * baseBoneLength;
-                    Base.LookAt(endPosition);
+                    var vec = endPosition - startPosition;
+                    var rot = Quaternion.FromToRotation(Vector3.up, vec);
                     Start.position = startPosition;
-                    Start.LookAt(endPosition);
+                    Start.rotation = rot;
+                    Middle.position = startPosition + vec.normalized * baseBoneLength;
+                    Middle.rotation = rot;
                     End.position = endPosition;
-                    End.rotation = Quaternion.identity;
+                    End.rotation = rot;
                 }
 
                 public void DrawGizmos()
                 {
-                    Handles.SphereHandleCap(0, Base.position, Quaternion.identity, 0.005f, EventType.Repaint);
                     Handles.SphereHandleCap(0, Start.position, Quaternion.identity, 0.005f, EventType.Repaint);
-                    Handles.Label(Base.position, "Base");
+                    Handles.SphereHandleCap(0, Middle.position, Quaternion.identity, 0.005f, EventType.Repaint);
                     Handles.Label(Start.position, "Start");
+                    Handles.Label(Middle.position, "Middle");
                     Handles.Label(End.position, "End");
-                    Handles.DrawLine(Start.position, End.position);
+                    Handles.DrawLine(Middle.position, End.position);
                 }
             }
 
@@ -93,16 +96,16 @@ namespace Narazaka.VRChat.BreastPBAdjuster
 
                 public bool Valid { get => Parent != null; }
 
-                public Transform Breast_L_base { get => Bones.TryGetValue(Bone.Breast_L_base, out var value) && value != null ? value : Parent.Find("Breast_L_base"); }
-                public Transform Breast_L { get => Bones.TryGetValue(Bone.Breast_L, out var value) && value != null ? value : Parent.Find("Breast_L_base/Breast_L"); }
-                public Transform Breast_L_end { get => Bones.TryGetValue(Bone.Breast_L_end, out var value) && value != null ? value : Parent.Find("Breast_L_base/Breast_L/Breast_L_end"); }
+                public Transform Breast_L { get => Bones.TryGetValue(Bone.Breast_L_base, out var value) && value != null ? value : Parent.Find("Breast_L"); }
+                public Transform Breast_L_middle { get => Bones.TryGetValue(Bone.Breast_L, out var value) && value != null ? value : Parent.Find("Breast_L/Breast_L_middle"); }
+                public Transform Breast_L_end { get => Bones.TryGetValue(Bone.Breast_L_end, out var value) && value != null ? value : Parent.Find("Breast_L/Breast_L_middle/Breast_L_end"); }
 
-                public Transform Breast_R_base { get => Bones.TryGetValue(Bone.Breast_R_base, out var value) && value != null ? value : Parent.Find("Breast_R_base"); }
-                public Transform Breast_R { get => Bones.TryGetValue(Bone.Breast_R, out var value) && value != null ? value : Parent.Find("Breast_R_base/Breast_R"); }
-                public Transform Breast_R_end { get => Bones.TryGetValue(Bone.Breast_R_end, out var value) && value != null ? value : Parent.Find("Breast_R_base/Breast_R/Breast_R_end"); }
-                public BoneSet L { get => _L == null || !_L.Valid ? new BoneSet(Breast_L_base, Breast_L, Breast_L_end) : _L; }
+                public Transform Breast_R { get => Bones.TryGetValue(Bone.Breast_R_base, out var value) && value != null ? value : Parent.Find("Breast_R"); }
+                public Transform Breast_R_middle { get => Bones.TryGetValue(Bone.Breast_R, out var value) && value != null ? value : Parent.Find("Breast_R/Breast_R_middle"); }
+                public Transform Breast_R_end { get => Bones.TryGetValue(Bone.Breast_R_end, out var value) && value != null ? value : Parent.Find("Breast_R/Breast_R_middle/Breast_R_end"); }
+                public BoneSet L { get => _L == null || !_L.Valid ? new BoneSet(Breast_L, Breast_L_middle, Breast_L_end) : _L; }
                 BoneSet _L;
-                public BoneSet R { get => _R == null || !_R.Valid ? new BoneSet(Breast_R_base, Breast_R, Breast_R_end) : _R; }
+                public BoneSet R { get => _R == null || !_R.Valid ? new BoneSet(Breast_R, Breast_R_middle, Breast_R_end) : _R; }
                 BoneSet _R;
             }
 
@@ -148,11 +151,11 @@ namespace Narazaka.VRChat.BreastPBAdjuster
             {
                 using (var check = new EditorGUI.ChangeCheckScope())
                 {
-                    var scale = Handles.ScaleHandle(main.Start.localScale, main.Start.position, main.Start.rotation, 0.02f);
+                    var scale = Handles.ScaleHandle(main.Middle.localScale, main.Middle.position, main.Middle.rotation, 0.02f);
                     if (check.changed)
                     {
-                        main.Start.localScale = scale;
-                        sub.Start.localScale = scale;
+                        main.Middle.localScale = scale;
+                        sub.Middle.localScale = scale;
                     }
                 }
             }
@@ -161,10 +164,10 @@ namespace Narazaka.VRChat.BreastPBAdjuster
             {
                 using (var check = new EditorGUI.ChangeCheckScope())
                 {
-                    var scale = main.Start.localScale;
+                    var scale = main.Middle.localScale;
                     var avgScale = (scale.x + scale.y + scale.z) / 3f;
                     var radiusEnd = Handles.RadiusHandle(Quaternion.identity, main.End.position, main.PB.radius * avgScale);
-                    var radiusStart = Handles.RadiusHandle(Quaternion.identity, main.Start.position, main.PB.CalcRadius(0.5f) * avgScale);
+                    var radiusStart = Handles.RadiusHandle(Quaternion.identity, main.Middle.position, main.PB.CalcRadius(0.5f) * avgScale);
                     if (check.changed)
                     {
                         var curve = new AnimationCurve(new Keyframe { time = 0, value = 0 }, new Keyframe { time = 0.5f, value = radiusStart / radiusEnd}, new Keyframe { time = 1, value = 1});
